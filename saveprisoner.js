25cedc812b6a0630951bf5b17061c1be41da2ac9
Levi-Huynh <Levi_huynh@yahoo.com>
2020-02-25 20:54:25 -0800

    savepr

---
+++ saveprisoner.js
@@ -0,0 +1,26 @@
+/**
+  
+  jail = [...prisoners]
+  treats = [...treats] for prisoners
+  chair (circular), sequentially ordered
+  start=rand(chairNumber)
+  end= start-end
+  last pece of candy tastes bad
+
+  6 pieces of candy (length of arr)
+  4 prisioners (before restart back to start )
+  start = 2   (sequential order)
+  warn prisoner = arr[len-1]
+
+  2,3,4,1, /sToVER 2,3 (length= 6, start =2, 4 prisoneers= 1 round (2d array?) 2D ARRAY, WITH SUB ARRAYS, TOTAL AMOUNT ELEMENTS = PIECES OF CANDY)
+
+ OUTPUT= integer, chair number/prision to warn
+  INPUT = n (int) # prisoners, m(int) number of sweets, s(int) chair number to start
+  
+ */
+
+
+function savePris() {
+
+}
+
