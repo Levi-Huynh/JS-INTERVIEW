1bf9e54ab5f58a1b13cc38993b495387028b875f
Levi-Huynh <Levi_huynh@yahoo.com>
2020-04-25 21:06:14 -0700

    increasing bst

--- revLL.py
+++ revLL.py
@@ -16,3 +16,30 @@
             head = next_node 
         #return prev based on standout/output
         return prev
+
+        class Solution:
+    def increasingBST(self, root: TreeNode) -> TreeNode:
+        # get most left
+        
+        if not root:
+            return root
+        
+        curr= root
+        while curr.left:
+            #iterate
+            curr = curr.left
+        
+        #curr should be most left here
+        #curr = root
+        #return left, null in betweeen each & r, sorted 
+        #root now is most left curr 
+        root = curr
+        print(f"test {root.left}. c: {curr}")
+        while curr.left and curr.right:
+            for i in range(0, 10):
+               
+                curr.left= None
+                curr.right= #
+            curr = curr.right 
+        
+        return curr
