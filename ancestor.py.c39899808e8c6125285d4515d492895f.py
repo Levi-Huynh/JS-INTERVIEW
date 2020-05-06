768e5577fd8f3f26c244154be9d9fd5a348f6171
Levi-Huynh <Levi_huynh@yahoo.com>
2020-05-05 21:07:04 -0700

    balanced tree

---
+++ ancestor.py.c39899808e8c6125285d4515d492895f.py
@@ -0,0 +1,398 @@
+
+""" 2. Build graph
+-nodes: parents /child
+-edges: relationship between parent/child
+-directed: parent can only be parent, child can only be child
+-acyclic
+- node on avg connected to more than 1/2 other nodes?: sparse
+- DFS- find the farthest relationship/path
+
+Given the dataset and the ID of an
+individual in the dataset, returns their earliest known
+ancestor – the one at the farthest distance from the input
+individual.
+
+If there is more than one ancestor tied for "earliest",
+return the one with the lowest numeric ID.
+
+If the input individual has no parents, the function should
+return -1.
+
+#if vertext has no more neighbors
+
+
+Assuming a bit more memory usage is not a problem and if the first
+item of your tuple is hashable, you can create a dict out of your
+list of tuples and then looking up the
+value is as simple as looking up a key from the dict
+
+test_ancestors = [(1, 3), (2, 3), (3, 6), (5, 6), (5, 7),
+                   (4, 5), (4, 8), (8, 9), (11, 8), (10, 1)]
+^ if has children, it is in ancester[tuple#][0]
+^ if has parent it is in ancester[tuple#][1], if start is not in ancester[i][1], has no parent
+^ track through list to check of parent has parent
+^ once parent no longer has parent, return that vertex, thats the farthest ancestor
+
+^ Extra:count the number of times its in ancester[i][1] == number of ancestors
+ """
+
+
+from util import Stack, Queue
+
+
+def get_parents(ancestors, start):
+    arr_par = []
+    for i in range(len(ancestors)):
+        print("get parents", start, ancestors[i], ancestors[i][1])
+        if start == ancestors[i][1]:
+            print("start found", start, ancestors[i][1])
+            arr_par.append(ancestors[i][0])
+
+    if len(arr_par) > 0:
+        return arr_par
+    else:
+        return None
+
+
+def earliest_ancestor(ancestors, starting_node):
+    s = Stack()
+    s.push([starting_node])
+    visited = set()
+
+    # ancestors=[(), ()] is list of sets/tuples
+    while s.size() > 0:
+        # Pop the vertex from back/end of stack, keeps check all in stack in order
+        path = s.pop()
+        a = path[-1]  # last element in list
+        stack_size = s.size()
+
+        if a not in visited:  # only explore that node if not yet explored
+            visited.add(a)
+            amount_parents = len(visited)
+            print("popped", a, "path", path, visited,
+                  stack_size, s, amount_parents)
+            find_parents = get_parents(ancestors, a)
+            # if find parents exist, add parents to stack
+            if find_parents is not None:
+                # needs to return list of parents
+                for parent in get_parents(ancestors, a):
+                    path_copy = path.copy()
+
+                    if parent is not None:
+                        # each parent of 6 added [6, 5, 3 ] 3 is popped & check for parent
+                        print("parent", parent)
+                        path_copy.append(parent)
+                    # [6,3,5] 3 or 5 is popped
+                    # check for neighbor of 3 or 5,
+                    # add parents of 3 to stack [6, 5, 2, 1]
+                    # pop off 1
+                    # check for parents of 1
+                        s.push(path_copy)
+                    # return negative one, if only one node was enqued& popped (resulting in len(visited)==1), & find_parent returns none
+            if find_parents is None and stack_size == 0 and amount_parents == 1:
+                return -1
+            if find_parents is None and stack_size == 0 and amount_parents > 1:
+                return a
+            # returns smaller parent by default, since parents in tupls are sorted by
+            # the smaller parent will always be popped last, since stack
+            # pops by last in (larger parent appended last)
+
+
+test_ancestors = [(1, 3), (2, 3), (3, 6), (5, 6), (5, 7),
+                  (4, 5), (4, 8), (8, 9), (11, 8), (10, 1)]
+
+print(earliest_ancestor(test_ancestors, 6))
+
+
+class Solution:
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        # iterate through ...
+        # do some logic based on the root val as iterate
+        while root:
+            if max(p.val, q.val) < root.val:
+                # for search move the root left (search smaller), if the MAX of the p&q are < root
+                root = root.left
+            elif min(p.val, q.val) > root.val:
+                # move search to right(search larger) if MIN of p&q are > root
+                root = root.right
+            else:
+                # if max or min bounds are correctly greater & correctly less than root.value
+                # than can be ancestor
+                return root
+        return None
+
+
+class Solution:
+    def preorder(self, root: 'Node') -> List[int]:
+
+        #         stack=[]
+        #         stack.append([root])
+        #         res=[]
+
+        #         while len(stack) > 0:
+        #             path=stack.pop()
+        #             v=path[-1]
+        #             if v is not None:
+        #                 res.append(v.val)
+
+        #             if v and v.children is not None:
+        #                 for child in v.children[::-1]:
+        #                     path_copy=path.copy()
+        #                     path_copy.append(child)
+        #                     stack.append(path_copy)
+
+        #         print(f"{res}")
+
+        #         return res
+
+        if not root:
+            return []
+        if not root.children:
+            return [root.val]
+        l = []
+        # dfs sorts by parent, then child, then next parent in bfs span
+        for child in root.children:
+            l += self.preorder(child)
+
+        l.insert(0, root.val)
+        return l
+
+        class Solution:
+
+    def diameterOfBinaryTree(self, root: TreeNode) -> int:
+
+        # stack of path for left node & stack of path for right node
+        def findChildren(root):
+            res = []
+            if root:
+                if root.left:
+                    res.append(root.left)
+                if root.right:
+                    res.append(root.right)
+                if len(res) > 0:
+                    return res
+                else:
+                    return None
+
+        def dfs(root, res):
+
+            stack = []
+            stack.append([root])
+
+            while len(stack) > 0:
+                path = stack.pop()
+
+                res.append(path)
+                v = path[-1]
+
+                if findChildren(v) is not None:
+                    for child in findChildren(v):
+                        path_copy = path.copy()
+                        path_copy.append(child)
+                        stack.append(path_copy)
+
+            return res
+
+        if root is None:
+            return 0
+        if root.left is None and root.right is None:
+            return 0
+
+        resL = []
+        resR = []
+        max_lenL = []
+        max_lenR = []
+        if root:
+            l = dfs(root.left, resL)
+            r = dfs(root.right, resR)
+
+        print(f" LEFTl: {l}\n RIGHT: {r} {r[-1]}")
+
+        if len(l) == 1 and l[-1] == [None]:
+            print("l is none")
+
+            for i in range(len(r)):
+                max_lenR.append(len(r[i]))
+            return max(max_lenR)
+
+        elif len(r) == 1 and r[-1] == [None]:
+            print("r is none")
+
+            for i in range(len(l)):
+                max_lenL.append(len(l[i]))
+            print(f"R is none max: {max(max_lenL)}")
+            return max(max_lenL)
+
+        elif len(l) >= 1 and len(r) >= 1:
+            print("equals")
+            for i in range(len(l)):
+                max_lenL.append(len(l[i]))
+            for i in range(len(r)):
+                max_lenR.append(len(r[i]))
+
+            max_len = max(max_lenL) + max(max_lenR)
+            print(f"maxL {max_lenL} maxR {max_lenR}")
+            return max_len
+
+        def isValid(self, s: str) -> bool:
+            # Open brackets must be closed by the same type of brackets.
+            # Open brackets must be closed in the correct order.
+        """
+        GIVEN STRING OF CHAR
+        RETURN BOOLEAN IF ABOVE CASES TRUE:
+        
+        pLAN
+        #BP
+        #HT 1) count of each open == count of each close
+        #2) all close index after 
+        #3) & close_index- open_index is odd num (should be even pairs of close & open in between, or none inbetween)
+        
+        """
+
+        if s is None:
+            return True
+
+        # HT 1) count of each open == count of each close
+        # store freq of brackets in dict
+        freq = {}
+
+        split = list(s)
+
+        print("S", split)
+        opposites = {'(': ')', ')': '(', '{': '}',
+                     '}': '{', '[': ']', ']': '['}
+
+        indexpos = {}
+
+        for char in split:
+            # index list of all cahars
+            indexpos[char] = [i for i in range(len(split)) if split[i] == char]
+
+        print(f"Indexpos {indexpos}")
+
+        check = {}
+        visited = set()
+
+        for k, v in indexpos.items():
+            if opposites[k] not in indexpos:
+                print()
+                return False
+            if len(indexpos[opposites[k]]) != len(indexpos[k]):
+                print("unequal char count")
+                return False
+
+        for k in indexpos:
+            index_check = []
+            if k not in visited:
+                visited.add(k)
+                visited.add(opposites[k])
+
+                for i in range(len(indexpos[k])):
+                    for j in range(len(indexpos[k])):
+                        index_check.append(
+                            indexpos[opposites[k]][i] - indexpos[k][j])
+            check[k] = index_check
+
+        print("check", check)
+
+        lenC = 0
+        for k, v in check.items():
+            if len(v) == 1:
+                if v[-1] % 2 == 0:
+                    return False
+            elif len(v) > 1:
+                for i in range(len(v)):
+                    if v[i] % 2 != 0:
+                        lenC += 1
+                if lenC != s.count(k):
+                    return False
+
+        return True
+
+
+class Solution:
+    def findTilt(self, root: TreeNode) -> int:
+        self.Lnodes = []
+        self.Rnodes=[]
+        self.Ltilt ={}
+        self.Rtilt={}
+        self.Lsum=[]
+        self.Rsum=[]
+        
+        rootR = 0
+        rootL=0
+        rootT =0
+        sumT=0
+    
+        
+        def traverseAll(root, stack):
+           
+            if not root:
+                return 0
+            if root.left :
+                #print(f"ROOT:{root.val} L{root.left.val}")
+                traverseAll(root.left,stack)
+             
+            if root.right:        
+                #print(f"ROOT:{root.val} R{root.right.val}")
+                traverseAll(root.right, stack)
+            stack.append(root) 
+            
+               
+        def individTilt(root):
+            store=[]
+            if not root: return 0
+            
+            if root.left:
+                
+               
+                individTilt(root.left)
+            
+            if root.right:
+                self.Rsum.append(root.right.val)
+                individTilt(root.right)
+                
+            store.append(root)
+            return store 
+            #d: {abs(sumL-sumR)},
+            print(f" sumL {self.Lsum} sumR {self.Rsum}")
+            #return abs(sumL-sumR)
+                
+        if not root:
+            return 0
+        
+        traverseAll(root.left, self.Lnodes ) #returns all L Nodes in stack  #THIS NEEDS TO OCCUR FOR ALL SUBPARENT ROOT'S NODES (with root.left & root.right as input)
+        traverseAll(root.right, self.Rnodes) #returns all R nodes in stack
+        #print(f"{self.Lnodes} {self.Rnodes}")
+        
+        
+        
+     
+        
+        while len(self.Lnodes) > 0:
+            vL=self.Lnodes.pop()
+            self.Ltilt[vL]=individTilt(vL)
+            
+        while len(self.Rnodes) > 0:
+            vR = self.Rnodes.pop()
+            self.Rtilt[vR]= individTilt(vR)
+        
+         def search(self, root):
+        # if we find the tree not height-balanced or the current
+        # subtree's height is 0, stop search and return 0
+        if not self.balanced or not root:
+            return 0
+
+        left = self.search(root.left)  # left subtree's height
+        right = self.search(root.right)  # right subtree's height
+        print(f"root:{root} left{left} right{right}")
+        if abs(left - right) > 1:  # if it's not height-balanced
+            self.balanced = False
+
+        # return current node's height
+        return max(left, right) + 1 #the height of the node & height of the l/R child 
+
+    def isBalanced(self, root):
+        self.balanced = True
+        self.search(root)
+        return self.balanced    
